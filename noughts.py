389e5236b541805eae33ecae033ece7c6c093975
jeff <jeff@jeff.jeff>
2023-12-04 15:54:55 +0000

    noughts + crosses

--- noughts.py
+++ noughts.py
@@ -6,17 +6,20 @@ from kivy.uix.button import Button
 
 class application(App):
    def build(self):
+#      self.grid = []
+#      for i in range(6):
+#         for j in range (6):
+#            self.grid.append((i,j))
       self.n = 'nought'
       layout = GridLayout(cols=5)
       for i in range(25):
         layout.add_widget(Button(text = '',on_press = self.handle_click))
       return layout
    def handle_click(self, instance):
-      print(self.n)
-      if self.n == 'nought':
+      if self.n == 'nought' and instance.text == '':
          instance.text = 'O'
          self.n = 'cross'
-      if self.n == 'cross':
+      elif self.n == 'cross' and instance.text == '':
          instance.text = 'X'
          self.n = 'nought'
 if __name__ == "__main__":
