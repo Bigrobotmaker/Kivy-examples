5ffb059356182ca3588b36ac982c3ed36a53ca36
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2023-12-12 17:40:59 +0000

    Update memory.py

--- memory.py
+++ memory.py
@@ -8,8 +8,14 @@ class application(App):
    def build(self):
       layout = GridLayout(cols=4)
       for i in range(16):
-        layout.add_widget(Label(text = 'click here'))
+        layout.add_widget(Label(text = 'click here'), on_touch_up = self.revealed)
       return layout
+   def revealed(self,instance,left):
+      self.layoutext = str(self.numbr)
+      clicky += 1
+      if clicky == 2:
+         self.layoutext = "click here!"
+         clicky = 0
 if __name__ == "__main__":
     myApp = application()
     myApp.run()
