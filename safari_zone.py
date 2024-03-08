7fea2a009a45a753971b71aeceba47d2cd9aa85f
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-03-08 15:26:05 +0000

    Update safari_zone.py

--- safari_zone.py
+++ safari_zone.py
@@ -72,8 +72,7 @@ class application(App):
                currentpokemon = 'kangaskahn'
                pkmnfleechance = 'low'
                pkmncatchchance = 'low'
-         self.layoutext = 'A wild', currentpokemon, 'appeared!'
-         self.encounter.text = str(self.layoutext)
+         self.encounter.text = 'A wild', currentpokemon, 'appeared!'
          visualfleechance = str(pkmnfleechance)
          self.fleechance.text = 'flee chance = ' + visualfleechance
          visualcatchchance = str(pkmncatchchance)
