fb42b258625aaba19f65b87d801a02352c74d0a9
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-03-08 15:24:12 +0000

    Update safari_zone.py

--- safari_zone.py
+++ safari_zone.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+import random
 visualfleechance = 'n/a'
 visualcatchchance = 'n/a'
 currentpokemon = 'n/a'
@@ -23,11 +24,59 @@ class application(App):
       global visualfleechance
       global currentpokemon
       if currentpokemon == 'n/a':
-         self.layoutext = 'A wild rattata appeared!'
+         a = random.randint(1,100)
+         if a <= 39:
+            tier = 'common'
+         if a >=40 and a <= 69:
+            tier = 'uncommon'
+         if a >= 70 and a <= 89:
+            tier = 'rare'
+         if a >= 90 and a <= 99:
+            tier = 'super_rare'
+         if a == 100:
+            tier = 'mew'
+            currentpokemon = 'mew'
+            pkmnfleechance = 'medium'
+            pkmncatchchance = 'very low'
+         if tier == 'common':
+            b = random.randint(1,2)
+            if b == 1:
+               currentpokemon = 'nidoran'
+               pkmnfleechance = 'low'
+               pkmncatchchance = 'high'
+            if b == 2:
+               currentpokemon = 'doduo'
+               pkmnfleechance = 'high'
+               pkmncatchchance = 'high'
+         if tier == 'uncommon':
+            b = random.randint(1,2)
+            if b == 1:
+               currentpokemon = 'tauros'
+               pkmncatchchance = 'medium'
+               pkmnfleechance = 'high'
+            if b == 2:
+               currentpokemon = 'venomoth'
+               pkmncatchchance = 'medium'
+               pkmnfleechance = 'high'
+         if tier == 'rare':
+            b= random.randint(1,3)
+            if b == 1:
+               currentpokemon = 'cubone'
+               pkmncatchchance = 'medium'
+               pkmnfleechance = 'low'
+            if b == 2:
+               currentpokemon = 'chansey'
+               pkmncatchchance = 'medium'
+               pkmnfleechance = 'high'
+            if b == 3:
+               currentpokemon = 'kangaskahn'
+               pkmnfleechance = 'low'
+               pkmncatchchance = 'low'
+         self.layoutext = 'A wild', currentpokemon, 'appeared!'
          self.encounter.text = str(self.layoutext)
-         visualfleechance = 'medium'
+         visualfleechance = str(pkmnfleechance)
          self.fleechance.text = 'flee chance = ' + visualfleechance
-         visualcatchchance = 'high'
+         visualcatchchance = str(pkmncatchchance)
          self.catchchance.text = 'catch chance = ' + visualcatchchance
 if __name__ == "__main__":
     myApp = application()
