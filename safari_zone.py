5db823908e28df05ebe62d18f57b2f557dd2e4ab
Bigrobotmaker <107852313+Bigrobotmaker@users.noreply.github.com>
2024-03-11 17:38:04 +0000

    Update safari_zone.py

--- safari_zone.py
+++ safari_zone.py
@@ -135,9 +135,13 @@ class application(App):
          visualcatchchance = str(pkmncatchchance)
          self.catchchance.text = 'catch chance = ' + visualcatchchance
    def safari(self,instance):
+      global rocks
+      global is_baited
       c = random.randint(1,100)
       odds = Pokemondata[currentpokemon]['catch']
       odds = odds + (5*rocks)
+      if is_baited == 'true':
+         odds = odds/2
       if c <= odds:
          print('catch success!')
       else:
